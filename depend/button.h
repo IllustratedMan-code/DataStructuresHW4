b41835f902f5b764067683263ffac66b9f77b165
David Lewis <davidalewis00@gmail.com>
2021-04-12 13:33:18 -0400

    fixed button ifndef

--- depend/button.h
+++ depend/button.h
@@ -1,14 +1,14 @@
-#ifndef button
-#define button
+#ifndef button_H
+#define button_H
 
 
 class button {
   protected:
   bool pressedbutton;   //button variable whether it is pressed or not
-    public:
+  public:
     int getbutton();    //pulls whether the button was press by a person
     void setbutton(bool pressedbutton);    //sets the button to the value true/false
-    void buttonopen(int floornumber, bool pressed button);  //checks if the button was "pressed" and outputs what floor
+    void buttonopen(int floornumber, bool pressedbutton);  //checks if the button was "pressed" and outputs what floor
 };
 
 #endif
--- depend/floor.h
+++ depend/floor.h
@@ -1,15 +1,17 @@
 #ifndef floor_H
 #define floor_H
 #include "button.h"
-#include "elevators"
-#include "person"
+#include "elevator.h"
+#include "person.h"
 #include <vector>
 #include <memory>
 
 class floor{
     int height;
-    std::vector<person> people;
-    bool UPorDown;
+    std::vector<person> people; //people on floor
+    button UP; // up button outside elevator
+    button DOWN; // down button outside elevator
+
 public:
     bool getstate();
     bool getDirection();
