a5b3d02f90300c47062ad655392f0973be472102
David Lewis <>
2021-04-21 13:48:04 +0000

    added clock and person code

---
+++ depend/person.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+#include "person.h"    // Person class definition
+#include "floor.h"     // Floor class definition
+#include "elevator.h"  // Elevator class definition
